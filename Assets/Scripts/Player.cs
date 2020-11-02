43b4892d2fcd8b694130aada4fd695fea3ff0e6b
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-11-02 20:13:41 +0900

    애니메이터 만들고 있는 중...

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float hAxis;
+    float vAxis;
+
+
+    public float speed;
+
+
+    Vector3 moveVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Right Analog Horizontal") + Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Right Analog Vertical") + Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+}
