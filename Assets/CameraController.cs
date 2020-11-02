11ee332851faa908386c45270d0ce3d650be2de2
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-11-02 19:17:14 +0900

    upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Quaternion TargetRotation;  // 최종적으로 축적된 Gap이 이 변수에 저장됨.
+    public Transform CameraVector;
+ 
+    public float RotationSpeed;        // 회전 스피드.
+    public float ZoomSpeed;            // 줌 스피드.
+    public float Distance;             // 카메라와의 거리.
+ 
+    private Vector3 AxisVec;           // 축의 벡터.
+    private Vector3 Gap;               // 회전 축적 값.
+    
+    private Transform MainCamera;      // 카메라 컴포넌트.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        //Zoom();
+        CameraRotation();
+    }
+ 
+    // 카메라 줌.
+    void Zoom()
+    {
+        Distance += Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed * -1;
+        Distance = Mathf.Clamp(Distance, 5f, 20f);
+ 
+        AxisVec = transform.forward * -1;
+        AxisVec *= Distance;
+        MainCamera.position = transform.position + AxisVec;
+    }
+ 
+    // 카메라 회전.
+    void CameraRotation()
+    {
+        if (transform.rotation != TargetRotation)
+            transform.rotation = Quaternion.Slerp(transform.rotation, TargetRotation, RotationSpeed * Time.deltaTime);
+ 
+        if (Input.GetMouseButton(1))
+        {
+            // 값을 축적.
+            Gap.x += Input.GetAxis("Mouse Y") * RotationSpeed * -1;
+            Gap.y += Input.GetAxis("Mouse X") * RotationSpeed;
+ 
+            // 카메라 회전범위 제한.
+            Gap.x = Mathf.Clamp(Gap.x, -5f, 85f);
+            // 회전 값을 변수에 저장.
+            TargetRotation = Quaternion.Euler(Gap);
+ 
+            // 카메라벡터 객체에 Axis객체의 x,z회전 값을 제외한 y값만을 넘긴다.
+            Quaternion q = TargetRotation;
+            q.x = q.z = 0;
+            CameraVector.transform.rotation = q;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+
+    Rigidbody rigidbody;
+
+    Vector3 movement;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Right Analog Horizontal") + Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Right Analog Vertical") + Input.GetAxisRaw("Vertical");
+        Run(h, v);
+    }
+
+    void Run(float h, float v)
+    {
+        movement.Set(h, 0, v); // ℝ^3 Vector movement의 값을 (h, 0, v)로 설정.
+
+        // .normalized는 크기가 1인 방향벡터를 반환.
+        movement = movement.normalized * speed * Time.deltaTime;
+        // movement를 (h, 0, v)의 방향벡터를 speed만큼 곱한걸 dt 만큼 곱한 값으로 설정.
+
+        rigidbody.MovePosition(transform.position + movement);
+        // rigidbody의 위치를 원래 위치에서 movement의 값만큼 더함.
+
+        //Gamepad.current.SetMotorSpeeds(0.25f, 0.75f);
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+                
+    }
+}
